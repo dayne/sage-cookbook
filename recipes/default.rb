a07d0e920c7182c925eee84fb7165746566300fd
Dayne Broderson <broderson@gmail.com>
2017-02-25 16:03:49 -0900

    working on deb/ubuntu - npm failing on pi

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,20 +7,19 @@
 # SAGE2 deps
 # NodeJS :
 
-include_recipe "nodejs"
+include_recipe 'nodejs'
 
 pkgs = %w(ghostscript imagemagick libcap2-bin libnss3-tools
-         libimage-exiftool-perl git-core)
+          libimage-exiftool-perl git-core)
 
 case node['platform']
 when /debian/
-when /raspbian/
   pkgs << 'chromium-browser-l10n'
+when /raspbian/
 else
   pkgs << 'chromium-browser'
 end
 package pkgs
-
 
 repo = node['sage2']['repository']       # https://bitbucket.org/sage2/sage2.git
 branch = node['sage2']['branch']         # master
@@ -35,21 +34,21 @@ make_user = true
 
 if make_user
   group groupname do
-    action [ :create ]
+    action [:create]
   end
   user username do
     comment 'SAGE2 User'
     group groupname
     home install_path
     shell '/bin/bash'
-    action [ :create ]
+    action [:create]
   end
 end
 
 directory install_path do
   user username
   group groupname
-  action [ :create ]
+  action [:create]
 end
 
 git 'sage2' do
@@ -58,7 +57,7 @@ git 'sage2' do
   destination         install_path
   group               groupname      # node['sage2']['group'] # sage
   user                username       # node['sage2']['user'] # sage
-  action              [ :checkout ]  # defaults to :sync if not specified
+  action              [:checkout] # defaults to :sync if not specified
 
   if node['sage2']['branch'] != 'master'
     checkout_branch   node['sage2']['branch']
@@ -69,12 +68,12 @@ end
 bash 'sage2-keys' do
   user username
   group groupname
-  cwd File.join(install_path,'keys')
+  cwd File.join(install_path, 'keys')
   environment 'HOME' => install_path
   code <<-EOH
   ./GO-linux
   EOH
-  not_if { ::File.exist?(File.join(install_path,'.pki')) }
+  not_if { ::File.exist?(File.join(install_path, '.pki')) }
 end
 
 bash 'sage2-npm_install' do
@@ -85,15 +84,15 @@ bash 'sage2-npm_install' do
   code <<-EOH
   /usr/bin/npm install
   EOH
-  not_if { ::File.exist?(File.join(install_path,'.npm')) }
+  not_if { ::File.exist?(File.join(install_path, '.npm')) }
 end
 
-#cd install_path/keys
-#./GO-linux
-#cd ..
-#[path to node]/bin/npm install
-#start the server
-#[path to node]/bin/node server.js -l
+# cd install_path/keys
+# ./GO-linux
+# cd ..
+# [path to node]/bin/npm install
+# start the server
+# [path to node]/bin/node server.js -l
 
 # add sage2 as a systemd service
 # https://bitbucket.org/sage2/sage2/wiki/systemd%20startup
