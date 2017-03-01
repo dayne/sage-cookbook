0bd3baa2e1e1d16e7c32b8fd05299e3dec85923c
Dayne Broderson <broderson@gmail.com>
2017-03-01 14:49:55 -0900

    basic stub of tests ready

---
+++ test/integration/default/default_spec.rb
@@ -0,0 +1,8 @@
+
+# TODO add real verification tests for sage - this is just a stub
+describe package("git-core") do
+    it { should be_installed }
+end
+
+# things like: confirm user, dir, ownership creation good
+# ensure npm/nodejs available, certs were created, and such
